38b343cf9395264ce125786434c83623f8f27b06
Almas Kureshi <almaskureshi099@gmail.com>
2026-05-25 08:27:48 +0530

    strat deployment

--- frontend/src/lib/socket.js
+++ frontend/src/lib/socket.js
@@ -1,8 +1,3 @@
 import { io } from "socket.io-client";
 
-export const socket = io(
-	"http://localhost:5000",
-	{
-		withCredentials: true,
-	}
-);
+export const socket = io(import.meta.env.VITE_API_URL.replace("/api", ""));
